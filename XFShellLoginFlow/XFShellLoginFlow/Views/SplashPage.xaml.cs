fb9aa370265c8b730572d41c26081d0e82f1e6f9
Shuhey <shuhey.dev@gmail.com>
2020-06-02 21:37:08 +0900

    Add project files.

---
+++ XFShellLoginFlow/XFShellLoginFlow/Views/SplashPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFShellLoginFlow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+        
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Initialize something you want here
+            await Task.Delay(1500);
+
+            //Transition to FirstPage
+            App.Current.MainPage = new AppShell();
+        }
+    }
+}
